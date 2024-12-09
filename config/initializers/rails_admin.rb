9f2b15aa36c5e9f5b77d15ffdabcae69078a59fe
Klevion Xhoxhaj <klevionxhoxhaj@gmail.com>
2024-12-10 00:45:45 +0100

    configuration changes for admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,15 @@
 RailsAdmin.config do |config|
   config.asset_source = :importmap
+  
+  RailsAdmin.config do |config|
+    config.authenticate_with do
+      warden.authenticate!(scope: :user)
+    end
+    config.current_user_method(&:current_user)
+    config.authorize_with do
+      redirect_to main_app.root_path unless current_user&.admin?
+    end
+  end
 
   ### Popular gems integration
 
