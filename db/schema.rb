28029515c715154e39435335b4e19c2ca4a79b97
Klevion Xhoxhaj <klevionxhoxhaj@gmail.com>
2024-12-10 11:02:59 +0100

    UPDATED USER TABLE

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_07_133006) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_10_100018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_07_133006) do
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.boolean "admin"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
