e082141fea97f3ec17459b2386e88d59d2c3fbc6
Klevion Xhoxhaj <klevionxhoxhaj@gmail.com>
2024-12-10 03:14:24 +0100

    added email validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
   has_one :review, dependent: :destroy
   validates :first_name, presence: true
   validates :last_name, presence: true
-  
+  before_validation :downcase_email
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+
   def admin?
     self.admin
   end
@@ -11,4 +14,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+
+  def downcase_email
+    self.email = email.to_s.downcase.strip
+  end
 end
